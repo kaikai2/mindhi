38ee359ea5f7a44d19d1cea11a3742fa78ef6c6a
Zhang Zikai <zikaizhang@gmail.com>
2014-10-07 16:33:22 +0800

    pick color

--- public/js/mindhi.js
+++ public/js/mindhi.js
@@ -10,25 +10,75 @@ app.controller('MainCtrl', function($scope, $route, $routeParams) {
 });
 
 
-app.controller('CoverCtrl', function($scope, $route, $routeParams) {
-	$scope.name = "BookController";
-	$scope.params = $routeParams;
-});
+app.controller('CoverCtrl', ['$scope', '$window', function($scope, $window) {
 
-app.controller('MakePicSelectActionCtrl', function($scope, $route, $routeParams) {
-	$scope.name = "BookController";
-	$scope.params = $routeParams;
-});
+}]);
+
+app.controller('MakePicSelectActionCtrl', ['$scope', '$window', function($scope, $window) {
+
+	var g_imglist = [
+		"img/face/1.jpg",
+		"img/face/1.png",
+		"img/face/10.png",
+		
+		"img/face/11.jpg",
+		"img/face/11.png",
+		"img/face/12.jpg",
+		
+		"img/face/12.png",
+		"img/face/13.png",
+		"img/face/14.png",
+		
+	];
+    $scope.actions = [];
+    for(var i = 0; i < g_imglist.length; i++){
+        $scope.actions.push({
+            name: g_imglist[i],
+            image: g_imglist[0],
+        });
+    }
+	$scope.select = function(action){
+		$scope.cur.act = action.name;
+	};
+}]);
 
 app.controller('MakePicSelectBackgroundCtrl', function($scope, $route, $routeParams) {
 	$scope.name = "BookController";
-	$scope.params = $routeParams;
+	//$scope.cur.bg;
+	
+	$(document).ready(function() {
+		$('#colorpicker').farbtastic(function(color){
+			$scope.cur.bg = color;
+		});
+	});
 });
 
 
 app.controller('MakePicSelectFaceCtrl', function($scope, $route, $routeParams) {
-	$scope.name = "BookController";
-	$scope.params = $routeParams;
+	var g_imglist = [
+		"img/face/1.jpg",
+		"img/face/1.png",
+		"img/face/10.png",
+		
+		"img/face/11.jpg",
+		"img/face/11.png",
+		"img/face/12.jpg",
+		
+		"img/face/12.png",
+		"img/face/13.png",
+		"img/face/14.png",
+		
+	];
+    $scope.faces = [];
+    for(var i = 0; i < g_imglist.length; i++){
+        $scope.faces.push({
+            name: g_imglist[i],
+            image: g_imglist[0],
+        });
+    }
+	$scope.select = function(action){
+		$scope.cur.face = action.name;
+	};
 });
 
 app.controller('MakePicSelectTextCtrl', function($scope, $route, $routeParams) {
@@ -36,10 +86,15 @@ app.controller('MakePicSelectTextCtrl', function($scope, $route, $routeParams) {
 	$scope.params = $routeParams;
 });
 
-app.controller('PublishCtrl', function($scope, $route, $routeParams) {
-	$scope.name = "BookController";
-	$scope.params = $routeParams;
-});
+app.controller('PublishCtrl', ['$scope', '$window', function($scope, $window) {
+	var width = $window.$(document).width() * 0.8;
+	var height = width * 3 / 2;
+	$window.$('#myCanvas').width(width);
+	$window.$('#myCanvas').height(height);
+	
+	
+	$scope.myCanvas
+}]);
 
 app.config(function($routeProvider, $locationProvider) {
 	$routeProvider.
